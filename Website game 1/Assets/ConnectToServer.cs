fa90e8284db920ceea7030ba553efcd290305a6a
SaadZellag <saadzellagui.wb@gmail.com>
2024-06-22 17:38:41 -0400

    added overlay :D

--- Website game 1/Assets/ConnectToServer.cs
+++ Website game 1/Assets/ConnectToServer.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
+using Photon.Realtime;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
+    public static string userId = null;
+
     void Start()
     {
+        if (userId != null)
+        {
+            AuthenticationValues auth = new AuthenticationValues(userId);
+            PhotonNetwork.AuthValues = auth;
+            Debug.Log("Set the values to " + auth);
+            PhotonNetwork.KeepAliveInBackground = 3600;
+        }
         PhotonNetwork.ConnectUsingSettings();
     }
 
