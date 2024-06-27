454093e06410219668c1bc28ab61dcb690fef2fe
Andres Munoz <ganonx9@gmail.com>
2024-06-27 16:15:22 -0400

    Respawn Ball delay and ball control

--- Website game 1/Assets/BallScript.cs
+++ Website game 1/Assets/BallScript.cs
@@ -8,12 +8,19 @@ public class BallScript : MonoBehaviour
     private Rigidbody2D rigid;
     void Awake()
     {
-        rigid=gameObject.GetComponent<Rigidbody2D>();        
+        rigid=gameObject.GetComponent<Rigidbody2D>();
     }
 
     public void setMoveDirection(int direction) {
+        StartCoroutine(helper(direction));
+        
+    }
+    private IEnumerator helper(int direction)
+    {
         rigid.transform.position = Vector2.zero;
-        rigid.velocity = new Vector2(direction * 5, Random.Range(-5,5));
+        rigid.velocity = Vector2.zero;
+        yield return new WaitForSeconds(0.5f);
+        rigid.velocity = new Vector2(direction * 5, Random.Range(-5, 5));
         Debug.Log("this ran");
     }
 
